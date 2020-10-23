d0b2e25d523ceca9376eb1940fc22fb8e16ebd22
chethanjs97 <chethanjschethu@gmail.com>
2020-10-23 08:08:07 +0530

    Added : Selecting Choice to do operation

--- src/com/addressbook/AddressBook.java
+++ src/com/addressbook/AddressBook.java
@@ -1,9 +1,20 @@
 package com.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBook {
 
 	public static void main(String[] args) {
 		System.out.println("WELCOME TO ADDRESS BOOK ");
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Select from the options below");
+		System.out.println("1.OPEN Address Book");
+		System.out.println("2.CREATE An Address Book");
+		System.out.println("3.DELETE An Address Book");
+		System.out.println("4.DELETE All Address Books");
+		System.out.println("5.EXIT");
+		int choice1=sc.nextInt();
 
 	}
 
